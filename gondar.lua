0c34de7d5caec86773f9aa4c13aca4e12da016f7
xiamubobby <xiamubobby@gmail.com>
2016-02-02 01:58:25 +0800

    test0

--- gondar.lua
+++ gondar.lua
@@ -2,7 +2,8 @@ file = io.open("test", 'a')
 io.output(file)
 for i = 0, 5, 1 do
   io.write("a")
+  os.execute("git add .")
+  os.execute("git commit -m test"..i)
 end
-os.execute("git add .")
-os.execute("git commit -m test")
+
 os.execute("git push")
